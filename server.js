e07d2965fb6376aced097ae670aee36a0cd2bcdf
coderarife <freelancerarifar@gmail.com>
2022-09-27 18:13:59 +0600

    changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const auth = require("json-server-auth");
 const jsonServer = require("json-server");
 const express = require("express");
 const http = require("http");
@@ -45,7 +46,14 @@ app.db = router.db;
 
 app.use(middlewares);
 
+const rules = auth.rewriter({
+    users: 640,
+    conversations: 660,
+    messages: 660,
+});
 
+app.use(rules);
+app.use(auth);
 app.use(router);
 
 server.listen(port);
